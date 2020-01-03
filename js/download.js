fabe62c2519725726cbfc337c6d5b2842e92b78a
lining <lining201702@126.com>
2020-01-03 10:25:51 +0800

    update

--- js/download.js
+++ js/download.js
@@ -2,19 +2,9 @@
 function getBrowserType() {
   var Sys = {};
   var ua = navigator.userAgent.toLowerCase();
-  var s;
-  (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] :
-    (s = ua.match(/msie ([\d\.]+)/)) ? Sys.ie = s[1] :
-      (s = ua.match(/edge\/([\d\.]+)/)) ? Sys.edge = s[1] :
-        (s = ua.match(/firefox\/([\d\.]+)/)) ? Sys.firefox = s[1] :
-          (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? Sys.opera = s[1] :
-            (s = ua.match(/chrome\/([\d\.]+)/)) ? Sys.chrome = s[1] :
-              (s = ua.match(/version\/([\d\.]+).*safari/)) ? Sys.safari = s[1] : 0;
-
-
-  if (Sys.safari) {
+  if (ua.indexOf('iphone') > -1 || ua.indexOf('ipod') > -1 || ua.indexOf('ipad') > -1) {
     open('itms-services://?action=download-manifest&url=https://coding.net/u/lining2017/p/ipainfo/git/raw/master/manifest.plist')
   } else {
-    alert('请使用 Safari 浏览器下载')
+    alert('请使用 iOS 设备下载')
   }
 }
